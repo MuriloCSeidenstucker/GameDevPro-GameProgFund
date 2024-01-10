dd2a3cc46d5456f55767683deddae9d50e720c4a
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2024-01-10 16:37:15 -0300

    Solution: LangeChange

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,29 +1,56 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float horizontalSpeed = 0.03f;
-    [SerializeField] private float forwardSpeed = 0.1f;
-    [SerializeField] private float laneDistance = 2f;
+    [SerializeField] private float horizontalSpeed = 15f;
+    [SerializeField] private float forwardSpeed = 10f;
+    [SerializeField] private float laneDistanceX = 1.5f;
 
+    private Vector3 initialPosition;
     private float targetPositionX;
+
+    private float LeftLaneX => initialPosition.x - laneDistanceX;
+    private float RightLaneX => initialPosition.x + laneDistanceX;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+    }
 
     private void Update()
     {
+        ProcessInput();
+
         Vector3 currentPosition = transform.position;
 
-        if (Input.GetKeyDown(KeyCode.A) && targetPositionX >= 0)
-        {
-            targetPositionX -= laneDistance;
-        }
-        if (Input.GetKeyDown(KeyCode.D) && targetPositionX <= 0)
-        {
-            targetPositionX += laneDistance;
-        }
-
-        currentPosition.x = Mathf.Lerp(currentPosition.x, targetPositionX, horizontalSpeed*Time.deltaTime);
-        currentPosition += Vector3.forward * forwardSpeed * Time.deltaTime;
+        currentPosition.x = ProcessLaneMovement();
+        currentPosition.z = ProcessForwardMovement();
 
         transform.position = currentPosition;
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetPositionX -= laneDistanceX;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            targetPositionX += laneDistanceX;
+        }
+
+        targetPositionX = Mathf.Clamp(targetPositionX, LeftLaneX, RightLaneX);
+    }
+
+    private float ProcessForwardMovement()
+    {
+        return transform.position.z + forwardSpeed * Time.deltaTime;
+    }
+
+    private float ProcessLaneMovement()
+    {
+        return Mathf.Lerp(transform.position.x, targetPositionX, horizontalSpeed * Time.deltaTime);
     }
 }
