66bfb373ce51ee4fd0950b73f7eb24a8d047b84a
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2024-01-19 16:28:46 -0300

    Class: 48 implementing infinite level generation part 1

--- Assets/Runtime/Scripts/Level/EndlessTrackGenerator.cs
+++ Assets/Runtime/Scripts/Level/EndlessTrackGenerator.cs
@@ -3,32 +3,74 @@ using UnityEngine;
 
 public class EndlessTrackGenerator : MonoBehaviour
 {
+    [SerializeField] private PlayerController player;
     [SerializeField] private TrackSegment[] trackSegments;
     [SerializeField] private TrackSegment firstTrackPrefab;
+
+    [Header("Endless Generation Parameters")]
     [SerializeField] private int initialTrackCount = 10;
+    [SerializeField] private int minTracksInFrontPlayer = 3;
 
     private List<TrackSegment> currentSegments = new();
 
     private void Start()
     {
-        TrackSegment initialTrack = Instantiate(firstTrackPrefab, transform);
-        currentSegments.Add(initialTrack);
-        TrackSegment previousTrack = initialTrack;
+        SpawnTrackSegment(firstTrackPrefab, null);
+        SpawnTracks(initialTrackCount);
+    }
 
-        for (int i = 0; i < initialTrackCount; i++)
+    private void Update()
+    {
+        int trackPlayerIndex = -1;
+        for (int i = 0; i < currentSegments.Count; i++)
+        {
+            if (player.transform.position.z >= currentSegments[i].Start.position.z
+                && player.transform.position.z <= currentSegments[i].End.position.z)
+            {
+                trackPlayerIndex = i;
+            }
+        }
+
+        if (currentSegments.Count - (trackPlayerIndex + 1) < minTracksInFrontPlayer)
+        {
+            Debug.Log(currentSegments.Count - (trackPlayerIndex + 1));
+        }
+    }
+
+    private void SpawnTracks(int trackCount)
+    {
+        TrackSegment previousTrack = currentSegments.Count > 0
+            ? currentSegments[currentSegments.Count - 1]
+            : null;
+
+        for (int i = 0; i < trackCount; i++)
         {
             int randomIndex = Random.Range(0, trackSegments.Length);
-            TrackSegment currentTrack = Instantiate(trackSegments[randomIndex], transform);
-            currentTrack.transform.position = previousTrack.End.position
-                + (currentTrack.transform.position - currentTrack.Start.position);
-
-            foreach (var obstacleSpawner in currentTrack.ObstaclesSpawners)
-            {
-                obstacleSpawner.SpawnObstacle();
-            }
-
-            currentSegments.Add(currentTrack);
-            previousTrack = currentTrack;
+            TrackSegment randomTrack = trackSegments[randomIndex];
+            previousTrack = SpawnTrackSegment(randomTrack, previousTrack);
         }
+    }
+
+    private TrackSegment SpawnTrackSegment(TrackSegment randomTrack, TrackSegment previousTrack)
+    {
+        TrackSegment trackInstance = Instantiate(randomTrack, transform);
+
+        if (previousTrack != null)
+        {
+            trackInstance.transform.position = previousTrack.End.position
+            + (trackInstance.transform.position - trackInstance.Start.position);
+        }
+        else
+        {
+            trackInstance.transform.localPosition = Vector3.zero;
+        }
+
+        foreach (var obstacleSpawner in trackInstance.ObstaclesSpawners)
+        {
+            obstacleSpawner.SpawnObstacle();
+        }
+
+        currentSegments.Add(trackInstance);
+        return trackInstance;
     }
 }
